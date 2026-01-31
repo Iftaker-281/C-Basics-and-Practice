2748572bf9f96b1b615ba40aa1008d98ca48f604
Iftaker Ahmed Tahmid <ahmedeftakhertahmid@gmail.com>
2026-01-31 14:50:41 +0600

    Add switch case example in switch_case.c

---
+++ IF-Else/switch_case.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a = 3;
+    switch(a) {
+        case 1: printf("One"); break;
+        case 2: printf("Two"); break;
+        case 3: printf("Three"); break;
+        default: printf("Default");
+    }
+
+    return 0;
+}
